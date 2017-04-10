14296e8082b1e9e7054fa693bfab503dc8f3e5d9
dantesix <norbert.pospischek@gmail.com>
2017-04-10 16:23:17 +0200

    adasmda

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// add stuff here
+/*
+            <div class="pokemon-card pure-u-md-1-2">
+                <div class="poke-container">
+                    <div class="content-subhead">
+                        Bulbu
+                    </div>
+                    <div class="pure-g">
+                        <div class="poke-info pure-u-1-2">
+                            ez egy bulbu
+                        </div>
+                        <div class="poke-thumb pure-u-1-2">
+                            <img class="pure-img-responsive" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+*/
+
