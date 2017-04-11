03e340e91c581124cd62d9ead74512d22e8d5650
dantesix <norbert.pospischek@gmail.com>
2017-04-11 09:03:26 +0200

    asda

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,47 @@
             </div>
 */
 
+var parent = document.getElementById("pokemonList");
+pokeList.map(function(poke) {
+    addPokemon(poke);
+});
+
+function addPokemon (pokemon) {
+    var pokecard = document.createElement("div");
+    pokecard.className = "pokemon-card pure-u-md-1-2";
+    var pokecont = document.createElement("div");
+    pokecont.className = "poke-container";
+    pokecard.appendChild(pokecont);
+
+    var pokename = document.createElement("div");
+    pokename.className = "content-subhead";
+    pokename.appendChild(document.createTextNode(pokemon.name));
+    pokecont.appendChild(pokename);
+
+    var innerContainer = document.createElement("div");
+    innerContainer.className = "pure-g";
+    innerContainer.appendChild(getpokeinfo(pokemon.type));
+    innerContainer.appendChild(getpokethumb(pokemon.sprite));
+    pokecont.appendChild(innerContainer);
+
+    parent.appendChild(pokecard);
+}
+
+function getpokeinfo (types) {
+    var info = document.createElement("div");
+    info.className = "poke-info pure-u-1-2";
+    types.map(function (type) {
+        info.appendChild(document.createTextNode(type));
+    });
+    return info;
+}
+
+function getpokethumb (sprite) {
+    var thumb = document.createElement("div");
+    thumb.className = "poke-thumb pure-u-1-2";
+    var img = document.createElement("img");
+    img.className = "pure-img-responsive";
+    img.src = sprite;
+    thumb.appendChild(img);
+    return thumb;
+}
